3a6142e70fd3519d2a643610d20dbc2af50a71a0
Ferid Bedru <bedru.ferid@ju.edu.et>
2021-03-04 12:26:55 +0300

    updated database tables with migration

---
+++ database/migrations/2021_02_04_115234_create_job_positions_table.php
@@ -0,0 +1,41 @@
+<?php
+
+use Illuminate\Database\Migrations\Migration;
+use Illuminate\Database\Schema\Blueprint;
+
+class CreateJobPositionsTable extends Migration
+{
+    /**
+     * Run the migrations.
+     *
+     * @return void
+     */
+    public function up()
+    {
+        Schema::table('job_positions', function(Blueprint $table)
+        {
+            $table->increments('id');
+            $table->integer('organization_unit')->unsigned()->index();
+            $table->integer('job_category')->unsigned()->index();
+            $table->integer('job_type')->unsigned()->index();
+            $table->text('job_description')->nullable();
+            $table->string('position_code');
+            $table->string('position_id');
+            $table->integer('salary')->unsigned()->index();
+            $table->integer('status');
+            $table->integer('job_title_category')->unsigned()->index();
+            $table->timestamps();
+
+        });
+    }
+
+    /**
+     * Reverse the migrations.
+     *
+     * @return void
+     */
+    public function down()
+    {
+        Schema::drop('job_positions');
+    }
+}
